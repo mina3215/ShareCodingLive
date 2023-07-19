78905e59df58f40dfbb158e48c8a4ed6bee09875
진희솜 <heesom.hs@gmail.com>
2023-07-19 16:19:32 +0900

    [기능] access, refresh 토큰 발급
    - jjwt 사용
    - refresh의 만료시간 3달
    - access의 만료시간 1시간
    
    S09P12D109-62 S09P12D109-65

---
+++ backend/src/main/java/com/codragon/sclive/jwt/Jwt.java
@@ -0,0 +1,57 @@
+package com.codragon.sclive.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+
+public class Jwt {
+    // 현재 시간
+    private Date now = new Date();
+
+    // 헤더 설정
+    private final HashMap<String, Object> headerMap = new HashMap<>() {{
+        put("alg", "HS256");
+        put("typ", "JWT");
+    }};
+
+    // 시크릿 키 생성
+    private final String random256BitKey = "6v9y$B&E)H@MbQeThWmZq4t7w!z%C*F-";
+    private final SecretKey secretKey = Keys.hmacShaKeyFor(random256BitKey.getBytes());
+
+    public String createRefreshToken(String email, String nickname) {
+        // 100*60*60 - 1시간
+        Date exp = new Date(now.getTime() + 1000*60*60*24*30*3);
+
+        String refreshToken = Jwts.builder()
+                .setHeaderParams(headerMap)
+                .setExpiration(exp)
+                .setIssuedAt(now)
+                .claim("email", email)
+                .claim("nickname", nickname)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+
+        return refreshToken;
+    }
+
+
+    public String createAccessToken(String email, String nickname) {
+        // 100*60*60 - 1시간
+        Date exp = new Date(now.getTime() + 1000*60*60);
+
+        String accessToken = Jwts.builder()
+                .setHeaderParams(headerMap)
+                .setExpiration(exp)
+                .setIssuedAt(now)
+                .claim("email", email)
+                .claim("nickname", nickname)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+
+        return accessToken;
+    }
+}
