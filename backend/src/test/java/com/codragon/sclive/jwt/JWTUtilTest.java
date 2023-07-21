cd2d5195289ab687ce4a561786413e97161a9a99
진희솜 <heesom.hs@gmail.com>
2023-07-21 17:50:03 +0900

    [테스트] valid 기능 테스트와 nickname 반환 테스트

---
+++ backend/src/test/java/com/codragon/sclive/jwt/JWTUtilTest.java
@@ -0,0 +1,40 @@
+package com.codragon.sclive.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+@DisplayName("JWT Util 서비스")
+class JWTUtilTest {
+
+    @Autowired
+    Jwt jwt;
+
+    @Test
+    @DisplayName("JWT 검증")
+    void createAccessToken() {
+
+        final boolean result = true;
+
+        String accessToken = jwt.createAccessToken("test", "test");
+        boolean valid = jwt.validateToken(accessToken);
+
+        log.debug("result: {}, actual: {}", valid, true);
+        Assertions.assertEquals(valid, result);
+    }
+
+    @Test
+    @DisplayName("토큰 안의 닉네임 반환 테스트")
+    void checkKey() {
+        String result = "Test";
+
+        String accessToken = jwt.createAccessToken("test", "Test");
+        String nickname = jwt.getNicknameFromToken(accessToken);
+        log.info("result: {}, actual: {}", nickname, result);
+
+        Assertions.assertEquals(nickname, result);
+    }
+}
