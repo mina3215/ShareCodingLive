1e9702822d6dc4fae93b142cfbe148206d4e378a
김민수 <mskim.spring@gmail.com>
2023-07-25 21:50:39 +0900

    [이동] PasswordEncoder Bean 생성 코드 이동
    JwtAuthenticationFilter.java -> BackendApplication.java
    
    - 순환 참조 문제로 인해 이동
    - JwtAuthenticationFilter -> UserService -> SecurityConfig ->
      JwtAuthenticationFilter

--- backend/src/main/java/com/codragon/sclive/config/SecurityConfig.java
+++ backend/src/main/java/com/codragon/sclive/config/SecurityConfig.java
@@ -1,17 +1,15 @@
 package com.codragon.sclive.config;
 
 import com.codragon.sclive.filter.JwtAuthenticationFilter;
-import com.codragon.sclive.jwt.Jwt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
@@ -39,10 +37,5 @@ public class SecurityConfig {
                 .authenticated();
 
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 }
