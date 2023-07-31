51079403f01a671b269ec88c068ef3061a8e1031
김유정 <kyj8441@gmail.com>
2023-07-31 17:29:29 +0900

    [기능] OpenviduUtil 구현
    - openvidu를 사용하기 위한 유틸

--- backend/src/main/java/com/codragon/sclive/controller/ConferenceController.java
+++ backend/src/main/java/com/codragon/sclive/controller/ConferenceController.java
@@ -1,5 +1,6 @@
 package com.codragon.sclive.controller;
 
+import com.codragon.sclive.domain.UserEntity;
 import com.codragon.sclive.dto.ConferenceCreateReqDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -7,6 +8,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Api(value = "회의방 API", tags = {"Conference"})
@@ -21,10 +23,11 @@ public class ConferenceController {
             @ApiResponse(code = 500, message = "서버 오류")
     })
     @PostMapping("/create")
-    public ResponseEntity<String> create(@RequestHeader("Authorization") String accessToken, @RequestBody ConferenceCreateReqDto conferenceCreateReqDto){ //title과 시작 시간 입력받음
+    public ResponseEntity<String> create(@AuthenticationPrincipal UserEntity user, @RequestBody ConferenceCreateReqDto conferenceCreateReqDto){ //title과 시작 시간 입력받음
         // 서비스에서 방 생성 로직 처리
         // 결과로 방 링크 반환
-        String link = "";
+        String uuid = "";
+        String link = "https://sclive/join/room/"+uuid;
         return ResponseEntity.status(200).body(link);
     }
 
@@ -34,7 +37,7 @@ public class ConferenceController {
             @ApiResponse(code = 500, message = "서버 오류")
     })
     @GetMapping("/start")
-    public ResponseEntity<String> start(@RequestHeader("Authorization") String accessToken, @RequestParam String link){
+    public ResponseEntity<String> start(@AuthenticationPrincipal UserEntity user, @RequestParam String link){
         // conference를 isActive = 1로 변경한다.
         // openvidu 연결??
         // 방장 회의방 참가? 아니면 방장도 join으로?
@@ -47,7 +50,7 @@ public class ConferenceController {
             @ApiResponse(code = 500, message = "서버 오류")
     })
     @GetMapping("/join") // Todo : 참가자가 나갔다 들어왔다 할때 어떻게 처리할지
-    public ResponseEntity<String> join(@RequestHeader("Authorization") String accessToken, @RequestParam String link){
+    public ResponseEntity<String> join(@AuthenticationPrincipal UserEntity user, @RequestParam String link){
         // conference_history 생성
         // openvidu 참가?
         return ResponseEntity.status(200).body("SUCCESS");
@@ -59,7 +62,7 @@ public class ConferenceController {
             @ApiResponse(code = 500, message = "서버 오류")
     })
     @GetMapping("/end")
-    public ResponseEntity<String> end(@RequestHeader("Authorization") String accessToken, @RequestParam String uuid){
+    public ResponseEntity<String> end(@AuthenticationPrincipal UserEntity user, @RequestParam String uuid){
         // conference를 isActive = 0으로 변경한다.
         // openvidu 종료? 연결 끊기
         return ResponseEntity.status(200).body("SUCCESS");
---
+++ backend/src/main/java/com/codragon/sclive/dto/ConferenceCreateResDto.java
@@ -0,0 +1,13 @@
+package com.codragon.sclive.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ConferenceCreateResDto {
+
+    String uuid;
+
+    String link;
+}
