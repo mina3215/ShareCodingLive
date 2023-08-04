457712d84dd5a8f99e83e21b669c9abf3f219774
박민아 <icherom@naver.com>
2023-08-04 15:11:54 +0900

    [기능/삭제]
    - proxy 오류 삭제
    - FullScreen 기능 중간 수정

--- frontend/src/common/api/http-common.js
+++ frontend/src/common/api/http-common.js
@@ -2,8 +2,7 @@ import baseAxios from 'axios';
 
 // axios 객체 설정
 const axios = baseAxios.create({
-  baseURL: '/sharecodinglive', // 기본 URL
-  withCredentials: true, // 요청 시 인증정보(쿠키 등) 자동 포함
+  baseURL: 'https://i9d109.p.ssafy.io:8094/', // 기본 URL
   headers: {
     'Content-Type': 'application/json', // 요청의 Content-Type 을 json으로 지정
   },
