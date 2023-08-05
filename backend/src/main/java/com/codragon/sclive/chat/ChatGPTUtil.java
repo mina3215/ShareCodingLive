c2101608b628d2a3b717e98a165061fd469db185
김민수 <mskim.spring@gmail.com>
2023-08-05 20:12:22 +0900

    [리팩토링] chatGPT에게 보내는 3개의 요청 비동기적으로 처리
    - 제목
    - 주석
    - 요약

--- backend/src/main/java/com/codragon/sclive/chat/ChatGPTUtil.java
+++ backend/src/main/java/com/codragon/sclive/chat/ChatGPTUtil.java
@@ -3,22 +3,29 @@ package com.codragon.sclive.chat;
 
 import io.github.flashvayne.chatgpt.service.ChatgptService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ChatGPTUtil {
 
-    private String API_KEY = "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";
+    // TODO: endpoint, api-key 주입 받기
+    private static final String API_KEY = "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";
     private static final String ENDPOINT = "https://api.openai.com/v1/completions";
+
     private final ChatgptService chatgptService;
 
     public String generateText(String prompt, float temperature, int maxTokens) {
@@ -27,7 +34,7 @@ public class ChatGPTUtil {
         headers.set("Authorization", "Bearer " + API_KEY);
 
         Map<String, Object> requestBody = new HashMap<>();
-        requestBody.put("model","text-davinci-003");
+        requestBody.put("model", "text-davinci-003");
         requestBody.put("prompt", prompt);
         requestBody.put("temperature", temperature);
         requestBody.put("max_tokens", maxTokens);
@@ -39,24 +46,44 @@ public class ChatGPTUtil {
         return response.toString();
     }
 
-    public String getTitle(String code){
-        StringBuilder question = new StringBuilder("Could you please make a title for the code snippet below?\n");
+    @Async
+    public CompletableFuture<String> getTitle(String code) throws InterruptedException {
+
+        log.debug("start to generate code Title");
+
+//        StringBuilder question = new StringBuilder("Could you please make a title for the code snippet below?\n");
+        StringBuilder question = new StringBuilder("아래 코드를 해석해서 제목을 달아줘 \n");
         question.append(code);
+
         String title = chatgptService.sendMessage(question.toString());
-        return title;
+
+        return CompletableFuture.completedFuture(title);
     }
 
-    public String addComment(String code){
-        StringBuilder question = new StringBuilder("아래의 코드에 설명 주석을 달아줘. 코드와 주석 외에는 답변하지 말아줘 : \n");
+    @Async
+    public CompletableFuture<String> addComment(String code) throws InterruptedException {
+
+        log.debug("start to generate code Comment");
+
+//        StringBuilder question = new StringBuilder("아래의 코드에 설명 주석을 달아줘. 코드와 주석 외에는 답변하지 말아줘 : \n");
+        StringBuilder question = new StringBuilder("아래 코드를 해석해서 주석을 달아줘 \n");
         question.append(code);
+
         String commentCode = chatgptService.sendMessage(question.toString());
-        return commentCode;
+
+        return CompletableFuture.completedFuture(commentCode);
     }
 
-    public String getSummarize(String code) {
+    @Async
+    public CompletableFuture<String> getSummarize(String code) throws InterruptedException {
+
+        log.debug("start to generate code Summarization");
+
         StringBuilder question = new StringBuilder("아래의 코드에 대한 한줄 평을 남겨줘 : \n");
         question.append(code);
+
         String summarization = chatgptService.sendMessage(question.toString());
-        return summarization;
+
+        return CompletableFuture.completedFuture(summarization);
     }
 }
