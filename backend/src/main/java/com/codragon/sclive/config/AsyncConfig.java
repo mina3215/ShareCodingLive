c89aa69069a4f8d9487d8e4a64197834cb659c51
김민수 <mskim.spring@gmail.com>
2023-08-05 18:49:13 +0900

    [추가] AsyncConfig 추가

---
+++ backend/src/main/java/com/codragon/sclive/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.codragon.sclive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "task-executor", destroyMethod = "shutdown")
+    public ThreadPoolTaskExecutor taskExecutor() {
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("ChatGPT-Thread");
+        executor.initialize();
+
+        return executor;
+    }
+}
