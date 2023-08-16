033f3f49612333462e2ed389dfc112bc2354e317
박민아 <icherom@naver.com>
2023-08-16 17:25:26 +0900

    [추가] 단비 페이지 추가
    - 단비 예약 로직 변경한거 병합

--- frontend/src/features/pages/Newconference.js
+++ frontend/src/features/pages/Newconference.js
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { getToken } from '../../common/api/JWT-common';
 import { getUUIDLink } from '../meeting/meetingSlice';
+import { setReservationFalse } from './pagesSlice/pagesSlice';
 
 import { toast } from 'react-toastify';
 
@@ -158,6 +159,7 @@ const NewConference = (props) => {
         setReserved(!reserved);
         setUUID('reserved');
         setLink('reserved');
+        dispatch(setReservationFalse());
       })
       .catch((err) => {
         if (err.status === 403) {
--- frontend/src/features/pages/pagesSlice/pagesSlice.js
+++ frontend/src/features/pages/pagesSlice/pagesSlice.js
@@ -8,7 +8,6 @@ import axios from '../../../common/api/http-common';
 // 기록 data 가져오기.
 export const getCodeData = createAsyncThunk('GET_CODE_DATA', async (data, { rejectWithValue }) => {
   console.log(DataTransferItemList);
-  console.log(data.token,'하하하호호호');
   try {
     const response = await axios.get('/user/history/code', { 
       headers: {
@@ -24,7 +23,8 @@ export const getCodeData = createAsyncThunk('GET_CODE_DATA', async (data, { reje
 
 // 초기값 설정
 const initialState = {
-  course: {}
+  course: {},
+  isLoadReservation: false,
 };
 
 // 리덕스 슬라이스 생성
@@ -39,13 +39,14 @@ const pagesSlice = createSlice({
       state.course = action.payload;
       console.log(state.course);
     },
+    setReservationFalse: (state) => {
+      state.isLoadReservation = !state.isLoadReservation;
+    },
   },
   extraReducers: (builder) => {
     builder
       .addCase(getCodeData.fulfilled, (state, actions) => {
         state.course = actions.payload[0].courses[0];
-
-        console.log('나 저장 잘 했엉 ',state.course);
       })
       .addCase(getCodeData.rejected, (state)=>{
         state.course = null;
@@ -53,5 +54,5 @@ const pagesSlice = createSlice({
     }
 });
 
-export const { resetCodeData, setCourse } = pagesSlice.actions;
+export const { resetCodeData, setCourse, setReservationFalse } = pagesSlice.actions;
 export default pagesSlice.reducer;
