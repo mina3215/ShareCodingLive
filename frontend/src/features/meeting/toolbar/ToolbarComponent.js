d96991c8d80da7f53222cfcc911715c2b0d34583
박민아 <icherom@naver.com>
2023-08-17 09:54:42 +0900

    [수정] 호스트 명단 오류 해결

--- frontend/src/features/meeting/toolbar/ToolbarComponent.js
+++ frontend/src/features/meeting/toolbar/ToolbarComponent.js
@@ -94,7 +94,9 @@ export default class ToolbarComponent extends Component {
   }
 
   handsUp() {
-    this.props.handsUp();
+    if(!this.props.user.isHost()){
+      this.props.handsUp();
+    }
   }
 
   micStatusChanged() {
